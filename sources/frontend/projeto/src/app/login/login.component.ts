a0166a069347cbfc0b826e579c7863ec6f62eeec
Wesley Gomes <wesley.gomes@wunderman.com>
2017-05-09 20:47:11 -0300

    Instalação materialize.

--- sources/frontend/projeto/src/app/login/login.component.ts
+++ sources/frontend/projeto/src/app/login/login.component.ts
@@ -2,14 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  template:`
-    <form action="#" method="post">
-      <label>Login:</label>
-      <input type="text" name="username" value="" />
-      <label>Senha:</label>
-      <input type="password" name="password" value=""  />
-      <input type="submit" name="login" value="Log In" />
-    </form>
-  `
+  templateUrl: './login.component.html',
 })
 export class LoginComponent { }
