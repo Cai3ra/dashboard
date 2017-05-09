ece52725c477db08f95e3de48ca72d67cffae6c3
LATAM\Jonaderson <jonaderson.moura@wunderman.com>
2017-05-09 17:54:58 -0300

    criei o componente de Login.

--- sources/frontend/Dashboard/src/app/app.component.ts
+++ sources/frontend/Dashboard/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 
+import { LoginComponent } from './login/login.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss', './login/login.component.scss'],
 })
 export class AppComponent {
   
