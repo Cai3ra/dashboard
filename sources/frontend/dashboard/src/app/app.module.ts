dce882e10c3a77d60af8d8b01eda8a527baa6122
Bruno Martins <am.bruno@live.com>
2017-05-10 15:15:49 -0300

    Iniciando projeto, componente de login criado, feito import materialize

---
+++ sources/frontend/dashboard/src/app/app.module.ts
@@ -0,0 +1,25 @@
+// import { MaterializeModule } from 'angular2-materialize';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent, LoginComponent]
+})
+export class AppModule { }
