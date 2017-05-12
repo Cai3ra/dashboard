a40b2ce1ddf8d539baf09281dc2a8bbc51c67563
Bruno Martins <am.bruno@live.com>
2017-05-12 17:50:08 -0300

    auth service

--- sources/frontend/dashboard/src/app/auth/auth.service.ts
+++ sources/frontend/dashboard/src/app/auth/auth.service.ts
@@ -3,10 +3,22 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
+  
+  private url:string;
 
-  constructor(private http: Http) { }
+  constructor(private http: Http) {
+    this.url = 'https://parseapi.back4app.com/classes/Person';
+  }
 
-  login(){
+  createAuthorizationHeader(headers: Headers) {
+    headers.append("X-Parse-Application-Id", "8O8NcvVl275ZAdo77ThMIY8HErYLksthxysJf8G8"); 
+    headers.append("X-Parse-REST-API-Key", "CMArOhznueRhV6pFP3VtAywJ7LoNTkpfAFhIZ1Lc"); 
+  }
 
+  create(url, data){
+    let header = new Headers();
+    this.createAuthorizationHeader(header);
+
+    return this.http.post(url, data, {headers: header});
   }
 }
