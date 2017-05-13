2c9f41229e87136dbf99f7fc9b7f8cb8e65610e6
Wesley Gomes <wesley.gomes70@gmail.com>
2017-05-13 00:55:19 -0300

    criando headers

--- sources/frontend/dashboard/src/app/auth/auth.service.ts
+++ sources/frontend/dashboard/src/app/auth/auth.service.ts
@@ -1,8 +1,24 @@
+import {Http, Headers} from '@angular/http';
 import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AuthService {
 
-  constructor() { }
+  private: url:string;
 
+  constructor(private http: Http) { 
+  	this.url = 'https://parseapi.back4app.com/classes/Person';
+  }
+
+  createAuthorizationHeader(headers: Headers) {
+	  headers.append("X-Parse-Application-Id", "8O8NcvVl275ZAdo77ThMIY8HErYLksthxysJf8G8"); 
+	  headers.append("X-Parse-REST-API-Key", "CMArOhznueRhV6pFP3VtAywJ7LoNTkpfAFhIZ1Lc"); 
+  }
+
+  create(url, data){
+  	let header = new Headers();
+  	this.createAuthorizationHeader(header);
+
+  	return this.http.post(url, data, {headers: header});
+  }
 }
